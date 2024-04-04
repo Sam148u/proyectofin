536bfdcec0ef27764549d14066f9542e985c3f66
Stiven <stivenpoveda30sistema@gmail.com>
2024-04-03 23:23:17 -0500

    commit2

--- pg.js
+++ pg.js
@@ -1,11 +1,20 @@
 const botonModoOscuro = document.getElementById('modoOscuro');
-let modoOscuroActivado = false;
+let modoOscuroActivado = localStorage.getItem('modoOscuro') === 'true';
+
+document.body.style.backgroundColor = modoOscuroActivado ? '#1f1f1f' : '#F3E5D8';
+document.body.style.color = modoOscuroActivado ? '#f1eded' : '#333333';
 
 botonModoOscuro.addEventListener('click', () => {
     modoOscuroActivado = !modoOscuroActivado;
-    document.body.style.backgroundColor = modoOscuroActivado ? '#333' : '#fff';
-    document.body.style.color = modoOscuroActivado ? '#fff' : '#333';
+    document.body.style.backgroundColor = modoOscuroActivado ? '#1f1f1f' : '#F3E5D8';
+    document.body.style.color = modoOscuroActivado ? '#f1eded' : '#333333';
+
+    localStorage.setItem('modoOscuro', modoOscuroActivado);
 });
 function redirigir() {
     window.location.href = "index.html";
 }
+document.querySelector("span:nth-child(2)").addEventListener("click", function() {
+    
+    window.location.href = "form.html";
+});
