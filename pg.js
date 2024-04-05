6c6620f7914230a28c39e7f6326486614d0470b8
Stiven <stivenpoveda30sistema@gmail.com>
2024-04-05 18:58:10 -0500

    commit5

--- pg.js
+++ pg.js
@@ -1,15 +1,22 @@
 const botonModoOscuro = document.getElementById('modoOscuro');
+const recipeCard = document.querySelector('.recipe-card-1');
 let modoOscuroActivado = localStorage.getItem('modoOscuro') === 'true';
 
-document.body.style.backgroundColor = modoOscuroActivado ? '#1f1f1f' : '#F3E5D8';
-document.body.style.color = modoOscuroActivado ? '#f1eded' : '#333333';
+document.body.style.backgroundColor = modoOscuroActivado? '#1f1f1f' : '#F3E5D8';
+document.body.style.color = modoOscuroActivado? '#f1eded' : '#333333';
+
+
+
+
 
 botonModoOscuro.addEventListener('click', () => {
-    modoOscuroActivado = !modoOscuroActivado;
-    document.body.style.backgroundColor = modoOscuroActivado ? '#1f1f1f' : '#F3E5D8';
-    document.body.style.color = modoOscuroActivado ? '#f1eded' : '#333333';
-
+    modoOscuroActivado =!modoOscuroActivado;
     localStorage.setItem('modoOscuro', modoOscuroActivado);
+
+    document.body.style.backgroundColor = modoOscuroActivado? '#1f1f1f' : '#F3E5D8';
+    document.body.style.color = modoOscuroActivado? '#f1eded' : '#333333';
+
+    
 });
 function redirigir() {
     window.location.href = "index.html";
@@ -18,8 +25,30 @@ document.querySelector("span:nth-child(2)").addEventListener("click", function()
     
     window.location.href = "form.html";
 });
-const recipeCard = document.getElementById('recipe1');
+const recipeCard1 = document.getElementById('recipe1');
 
-recipeCard.addEventListener('click', () => {
+recipeCard1.addEventListener('click', () => {
     window.location.href = 'recipe1.html';
+});
+const recipeCard2 = document.getElementById('recipe2');
+
+recipeCard2.addEventListener('click', () => {
+    window.location.href = 'recipe2.html';
+});
+const recipeCard3 = document.getElementById('recipe3');
+
+recipeCard3.addEventListener('click', () => {
+    window.location.href = 'recipe2.html';
+});const recipeCard4 = document.getElementById('recipe4');
+
+recipeCard2.addEventListener('click', () => {
+    window.location.href = 'recipe2.html';
+});const recipeCard5 = document.getElementById('recipe5');
+
+recipeCard5.addEventListener('click', () => {
+    window.location.href = 'recipe2.html';
+});const recipeCard6 = document.getElementById('recipe6');
+
+recipeCard6.addEventListener('click', () => {
+    window.location.href = 'recipe2.html';
 });
